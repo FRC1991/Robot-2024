97419188850bc81edf9a34fbdcb49659a64e39ec
mineable3 <emmet.spaeth@gmail.com>
2024-01-27 12:12:04 -0500

    working figure 8

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -130,9 +130,9 @@ public final class Constants {
     public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
     public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
 
-    public static final double kPXController = 1;
-    public static final double kPYController = 1;
-    public static final double kPThetaController = 1;
+    public static final double kPXController = 0.8;
+    public static final double kPYController = 0.8;
+    public static final double kPThetaController = 0.8;
 
     // Constraint for the motion profiled robot angle controller
     public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
