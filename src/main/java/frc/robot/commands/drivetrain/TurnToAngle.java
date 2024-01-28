4f7a962709025d25b977c7c7f80cb4fa0497c810
Emmet Spaeth <emmet.spaeth@gmail.com>
2024-01-27 22:23:52 -0500

    updating documentation

--- src/main/java/frc/robot/commands/drivetrain/TurnToAngle.java
+++ src/main/java/frc/robot/commands/drivetrain/TurnToAngle.java
@@ -12,13 +12,16 @@ public class TurnToAngle extends Command {
   private double angle, currentHeading;
   private DriveSubsystem m_DriveSubsystem;
   /** 
-   * Creates a command that minimizes the angle and doesn't end
+   * Minimizes difference between the robot heading and the target angle
+   * The command ends when the difference is below the tolerance
    * 
    * @param angle The angle between the target and the current heading of the robot
    * @param driveSubsystem The drive subsystem of the robot
   */
   public TurnToAngle(double angle, DriveSubsystem driveSubsystem) {
+  public TurnToAngle(double angle, double tolerance, DriveSubsystem driveSubsystem) {
     this.angle = angle;
+    this.tolerance = tolerance;
     this.m_DriveSubsystem = driveSubsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(driveSubsystem);
@@ -32,12 +35,15 @@ public class TurnToAngle extends Command {
   @Override
   public void execute() {
     currentHeading = m_DriveSubsystem.getHeading();
+
+    //optimizing angle for continous output
     if(currentHeading > (angle + 180)) {
       currentHeading -= 360;
     } else if(currentHeading < (angle - 180)) {
       currentHeading += 360;
     }
     
+    //driving of the motors
     if(currentHeading < 180) {
       m_DriveSubsystem.drive(0, 0, -0.2, false, true);
     } else if(currentHeading > 180) {
@@ -52,7 +58,7 @@ public class TurnToAngle extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if(Math.abs(angle - m_DriveSubsystem.getHeading()) <= 10) {
+    if(Math.abs(angle - m_DriveSubsystem.getHeading()) <= tolerance) {
       return true;
     } else {
       return false;
