53fe01ad4bcfe39cbcc119afbb9724bda9392483
mineable3 <emmet.spaeth@gmail.com>
2024-02-16 17:00:53 -0500

    filling Climber subsystem

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,11 +4,30 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ClimberConstants;
 
 public class Climber extends SubsystemBase {
+
+  private CANSparkMax climberMotor1, climberMotor2;
+
   /** Creates a new Climber. */
-  public Climber() {}
+  public Climber() {
+    climberMotor1 = new CANSparkMax(ClimberConstants.kClimberMotor1Id, MotorType.kBrushless);
+    climberMotor2 = new CANSparkMax(ClimberConstants.kClimberMotor2Id, MotorType.kBrushless);
+
+    climberMotor1.setIdleMode(IdleMode.kBrake);
+    climberMotor2.setIdleMode(IdleMode.kBrake);
+  }
+
+  public void setClimber(double speed) {
+    climberMotor1.set(speed);
+    climberMotor2.set(speed);
+  }
 
   @Override
   public void periodic() {
