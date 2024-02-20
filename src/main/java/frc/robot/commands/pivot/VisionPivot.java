495514e212c68835c73c6267d4b42689c72fa125
Emmet Spaeth <emmet.spaeth@gmail.com>
2024-02-19 22:31:25 -0500

    creating vision command for Pivot

---
+++ src/main/java/frc/robot/commands/pivot/VisionPivot.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.pivot;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Pivot;
+
+public class VisionPivot extends Command {
+
+  private AtomicReference<Double> ta;
+  private Pivot m_Pivot;
+
+  /** Creates a new VisionPivot. */
+  public VisionPivot(AtomicReference<Double> ta, Pivot pivot) {
+    this.ta = ta;
+    m_Pivot = pivot;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(pivot);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_Pivot.setPivot(ta.get());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
