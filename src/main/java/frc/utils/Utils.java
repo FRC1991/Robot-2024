be2aada1684c7cafc37b135b712236f62990a544
Emmet Spaeth <emmet.spaeth@gmail.com>
2024-06-23 12:23:56 -0400

    adding javadoc comment

--- src/main/java/frc/utils/Utils.java
+++ src/main/java/frc/utils/Utils.java
@@ -1,6 +1,12 @@
 package frc.utils;
 
 public class Utils {
+    /**
+     * All speeds that a motor is set to should be between -1 and 1.
+     * This method ensures that all values will be safe for a motor to run at.
+     * @param input The desired speed of a motor
+     * @return The desired speed normalized between [-1, 1]
+     */
     public static double normalize(double input) {
         if(input > 1) {
             return 1;
