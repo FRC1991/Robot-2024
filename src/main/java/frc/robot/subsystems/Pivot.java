0b22ea0a72961c4d2477482643d04f2f0473b9f7
Emmet Spaeth <emmet.spaeth@gmail.com>
2024-06-23 12:33:11 -0400

    refactoring Pivot extending Subsystem

--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -5,45 +5,109 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkBase.SoftLimitDirection;
+import com.revrobotics.REVLibError;
+import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.PivotConstants;
+import frc.utils.Utils;
 
-public class Pivot extends SubsystemBase {
+public class Pivot extends Subsystem {
 
   private CANSparkMax pivotMotor1, pivotMotor2;
+  private static Pivot m_Instance;
 
-  /** Creates a new Pivot. */
-  public Pivot() {
+  // Constructor is private to prevent multiple Pivots from being made
+  private Pivot() {
     pivotMotor1 = new CANSparkMax(PivotConstants.kPivotMotor1Id, MotorType.kBrushless);
     pivotMotor2 = new CANSparkMax(PivotConstants.kPivotMotor2Id, MotorType.kBrushless);
 
-    //TODO find the actual limit for these
-    pivotMotor1.setSoftLimit(SoftLimitDirection.kForward, PivotConstants.kForwardLimit);
-    pivotMotor2.setSoftLimit(SoftLimitDirection.kForward, PivotConstants.kForwardLimit);
+    // Motor is inverted because it faces the opposite direction of motor 1
+    pivotMotor2.setInverted(true);
 
-    //TODO find the actual limit for these
-    pivotMotor1.setSoftLimit(SoftLimitDirection.kReverse, PivotConstants.kBackLimit);
-    pivotMotor2.setSoftLimit(SoftLimitDirection.kReverse, PivotConstants.kBackLimit);
+    // Setting idle mode to brake so the pivot won't move during collisions
+    pivotMotor1.setIdleMode(IdleMode.kBrake);
+    pivotMotor2.setIdleMode(IdleMode.kBrake);
 
-    pivotMotor1.getEncoder().setPosition(0);
-    pivotMotor2.getEncoder().setPosition(0);
+    // Saving the desired settings
+    pivotMotor1.burnFlash();
+    pivotMotor2.burnFlash();
+
+    zeroMotorEncoders();
   }
 
+  /**
+   * 
+   * @return The main Pivot object
+   */
+  public static Pivot getInstance() {
+    if(m_Instance == null) {
+      m_Instance = new Pivot();
+    }
+    return m_Instance;
+  }
+
+  /**
+   * 
+   * @param speed The desired speed to run both motors at.
+   */
   public void setPivot(double speed) {
+    speed = Utils.normalize(speed);
     pivotMotor1.set(speed);
-    pivotMotor2.set(-speed);
+    pivotMotor2.set(speed);
   }
 
+  /**
+   * 
+   * @return The average encoder value between both motors internal relative encoders
+   */
   public double getEncoderPosition() {
     // Pivot motor 2 is subtracted because it is run in reverse
     return (pivotMotor1.getEncoder().getPosition() - pivotMotor2.getEncoder().getPosition()) / 2;
   }
 
-  public void zeroEncoders() {
+  /**
+   * Resets the relative encoder in both motors
+   */
+  public void zeroMotorEncoders() {
     pivotMotor1.getEncoder().setPosition(0);
     pivotMotor2.getEncoder().setPosition(0);
+  }
+
+  /**
+   * Stops movement in both motors
+   */
+  public void stop() {
+    pivotMotor1.stopMotor();
+    pivotMotor2.stopMotor();
+  }
+
+  /**
+   * 
+   * @return Is the subsystem is okay to operate
+   */
+  public boolean checkSubsystem() {
+    // Check CAN Id
+    if(pivotMotor1.getDeviceId() != PivotConstants.kPivotMotor1Id) {
+      return false;
+    } else if(pivotMotor2.getDeviceId() != PivotConstants.kPivotMotor2Id) {
+      return false;
+    }
+
+    // Check motor temp.
+    if(pivotMotor1.getMotorTemperature() >= 25) {
+      return false;
+    } else if(pivotMotor2.getMotorTemperature() >= 25) {
+      return false;
+    }
+
+    // Check last error/fault
+    if(pivotMotor1.getLastError() != REVLibError.kOk) {
+      return false;
+    } else if(pivotMotor2.getLastError() != REVLibError.kOk) {
+      return false;
+    }
+
+    return true;
   }
 
   @Override
